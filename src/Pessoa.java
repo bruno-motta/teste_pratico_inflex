20f0c7bb5d8eeb22d2599b9a1cd10390420131bc
Bruno Mota <contatobrunotr@gmail.com>
2026-04-18 12:14:03 -0300

    Implementas as classes Pessoa e Funcionario

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -5,4 +5,25 @@ public class Pessoa {
 
     String nome;
     LocalDateTime localDate;
+
+    public Pessoa(String nome, LocalDateTime localDate) {
+        this.nome = nome;
+        this.localDate = localDate;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public LocalDateTime getLocalDate() {
+        return localDate;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setLocalDate(LocalDateTime localDate) {
+        this.localDate = localDate;
+    }
 }
